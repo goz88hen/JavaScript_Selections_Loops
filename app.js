a25bd41f0b64adf445331282b66cf2e2597a59f6
Henry (Hendra) Gozali <123764602+goz88hen@users.noreply.github.com>
2023-07-06 18:49:29 -0700

    Completed all exercises.

--- app.js
+++ app.js
@@ -1,7 +1,83 @@
-console.log("Hello World!\n==========\n");
-
 // Exercise 1 Section
-console.log("EXERCISE 1:\n==========\n");
+
+for (let i = 0; i < 100; i+= 2)
+{
+    console.log(i);
+}
+// do{
+//     if(x % 2 !== 0){
+//         console.log(x);
+//     }
+
+//     x++
+// }while(x<100);
+
+// while(i < 100){
+//     if(i % 2 !== 0){
+//         console.log(i);
+//     }
+
+//     i++
+// }
+
 
 // Exercise 2 Section
-console.log("EXERCISE 2:\n==========\n");
+for (let i = 1; i < 100; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0 )
+    {
+        console.log(i, "FIZZBUZZ");
+    }else
+    {
+        if(i % 3 == 0){
+            console.log(i, "FIZZ");
+        }
+        if(i % 5 == 0){
+            console.log(i, "BUZZ");
+        }else{
+            console.log(i);
+        }
+    }
+
+    // for (let i = 1; i <= 100; i++){
+    //     let output = "";
+
+    //     if (i % 3 == 0){
+    //         output += "FIZZ";
+    //     }
+
+    //     if(i % 5 == 0){
+    //         output += "BUZZ";
+    //     }
+        
+    //     console.log(`${i} ${output}`);
+    // }
+}
+
+//Exercise 4 section 
+
+let value = Math.round((Math.random() * 500)); 
+// creates a random number between 0 and 500
+
+let n = Math.round(Math.random() * (500 - 100) + 100); 
+// creates a random number between 100 and 500
+
+for (let i = 1; i <= n; i++){
+    if ( i == value){
+    console.log(`Found ${value}!`);
+   break;}
+    }
+
+console.log(`Did not find ${value} within 1-${n}.`);
+
+//Exercise 5: Custom FIZZBUZZ
+let fizzDivisor = Math.round(Math.random() * (10 - 1) + 1); // creates a random number between 1 and 10
+let buzzDivisor = Math.round(Math.random() * (10 - 1) + 1); // creates a random number between 1 and 10
+let start = Math.round(Math.random() * (10 - 1) + 1); // creates a random number between 1 and 10
+let end = Math.round(Math.random() * (1000 - 100) + 100); // creates a random number between 100 and 1000
+
+console.log(start,end);
+
+for(let i = start; i <= end; i++){
+    console.log(i);
+}
